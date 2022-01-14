4ba2e2b75e33a9657c998481ad787539c9c76542
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-14 12:03:16 -0600

    functions update

--- src/components/moreforms.js
+++ src/components/moreforms.js
@@ -28,12 +28,29 @@ const MoreForm = (props) => {
         return str.length >= 8;
     }
 
-    // const pwMatch = (event) => {
-    //     if (event.target.value != event.target.password){
-    //         return;
-    //     }
-    // }
+    const pwMatch = (conPassword) => {
+        if (conPassword == form.password){
+            return true;
+        }
+    }
 
+    const lengthValidator = (input) => {
+        if(input.length > 2){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const allValid = (inputs) => {
+        return lengthValidator(inputs.firstName)
+        && lengthValidator(inputs.lastName)
+        && lengthValidator(inputs.email)
+        && lengthValidator(inputs.password)
+        && lengthValidator(inputs.conPassword)
+    }
+
+    
 
     return (
         <div>
@@ -41,37 +58,43 @@ const MoreForm = (props) => {
                 <div>
                     <h2>Validations</h2>
 
-                    {!nameValid(form.firstName) && <span style={{color:"red"}}>First Name must be 3 characters long</span>}
+                    {!nameValid(form.firstName) && form.firstName.length > 0 && <span style={{color:"red"}}>First Name must be 3 characters long</span>}
                     <div>
                         <label htmlFor="firstName">First Name</label>
                         <input name="firstName" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!nameValid(form.lastName) && <span style={{color:"red"}}>Last Name must be 3 characters long</span>}
+                    {!nameValid(form.lastName) && form.lastName.length > 0 && <span style={{color:"red"}}>Last Name must be 3 characters long</span>}
                     <div>
                         <label htmlFor="lastName">Last Name</label>
                         <input name="lastName" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!emailValid(form.email) && <span style={{color:"red"}}>Email must be 5 characters long</span>}
+                    {!emailValid(form.email) && form.email.length > 0 && <span style={{color:"red"}}>Email must be 5 characters long</span>}
                     <div>
                         <label htmlFor="email">Email</label>
                         <input name="email" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!pwValid(form.password) && <span style={{color:"red"}} >Password must be 8 characters long</span>}
+                    {!pwValid(form.password) && form.password.length > 0 && <span style={{color:"red"}} >Password must be 8 characters long</span>}
                     <div>
                         <label htmlFor="password">Password</label>
                         <input name="password" type="password" onChange={onChangeHandler} />
                     </div>
 
-                    {!pwValid(form.conPassword) && <span style={{color:"red"}}>Password must be 8 characters long</span>}
-                    {/* {!pwMatch(form.conPassword)} */}
+                    {!pwValid(form.conPassword) && form.conPassword.length > 0 && <span style={{color:"red"}}>Password must be 8 characters long</span>}
+                    {!pwMatch(form.conPassword) && form.conPassword.length > 0 && <span style={{color:"red"}}>Passwords must match</span>}
                     <div>
                         <label htmlFor="conPassword">Confirm Password</label>
                         <input name="conPassword" type="password" onChange={onChangeHandler} />
                     </div>
-                    <input type="submit" value="Create"></input>
+
+                    {
+                        allValid(form)
+                        ? <input type="submit" value="Create"></input>
+                        : <input type="submit" value="Create" disabled></input>
+                    }
+
                 </div>
             </form>
             <div>
