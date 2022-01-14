2abf2cac040882d66a2e99148ae1feddd5e4bb43
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-14 12:26:42 -0600

    css update

--- src/components/moreforms.js
+++ src/components/moreforms.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const MoreForm = (props) => {
     const [form, setForm] = useState({
         firstName: "",
@@ -16,6 +17,8 @@ const MoreForm = (props) => {
         })
     }
 
+
+
     const nameValid = (str) => {
         return str.length >= 3;
     }
@@ -29,13 +32,13 @@ const MoreForm = (props) => {
     }
 
     const pwMatch = (conPassword) => {
-        if (conPassword == form.password){
+        if (conPassword == form.password) {
             return true;
         }
     }
 
     const lengthValidator = (input) => {
-        if(input.length > 2){
+        if (input.length > 2) {
             return true;
         } else {
             return false;
@@ -44,13 +47,13 @@ const MoreForm = (props) => {
 
     const allValid = (inputs) => {
         return lengthValidator(inputs.firstName)
-        && lengthValidator(inputs.lastName)
-        && lengthValidator(inputs.email)
-        && lengthValidator(inputs.password)
-        && lengthValidator(inputs.conPassword)
+            && lengthValidator(inputs.lastName)
+            && lengthValidator(inputs.email)
+            && lengthValidator(inputs.password)
+            && lengthValidator(inputs.conPassword)
     }
 
-    
+
 
     return (
         <div>
@@ -58,41 +61,44 @@ const MoreForm = (props) => {
                 <div>
                     <h2>Validations</h2>
 
-                    {!nameValid(form.firstName) && form.firstName.length > 0 && <span style={{color:"red"}}>First Name must be 3 characters long</span>}
+                    {!nameValid(form.firstName) && form.firstName.length > 0 && <span className="danger">First Name must be 3 characters long</span>}
                     <div>
                         <label htmlFor="firstName">First Name</label>
                         <input name="firstName" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!nameValid(form.lastName) && form.lastName.length > 0 && <span style={{color:"red"}}>Last Name must be 3 characters long</span>}
+                    {!nameValid(form.lastName) && form.lastName.length > 0 && <span className="danger">Last Name must be 3 characters long</span>}
                     <div>
                         <label htmlFor="lastName">Last Name</label>
                         <input name="lastName" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!emailValid(form.email) && form.email.length > 0 && <span style={{color:"red"}}>Email must be 5 characters long</span>}
+                    {!emailValid(form.email) && form.email.length > 0 && <span className="danger">Email must be 5 characters long</span>}
                     <div>
                         <label htmlFor="email">Email</label>
                         <input name="email" type="text" onChange={onChangeHandler} />
                     </div>
 
-                    {!pwValid(form.password) && form.password.length > 0 && <span style={{color:"red"}} >Password must be 8 characters long</span>}
+                    {!pwValid(form.password) && form.password.length > 0 && <span className="danger" >Password must be 8 characters long</span>}
                     <div>
                         <label htmlFor="password">Password</label>
                         <input name="password" type="password" onChange={onChangeHandler} />
                     </div>
 
-                    {!pwValid(form.conPassword) && form.conPassword.length > 0 && <span style={{color:"red"}}>Password must be 8 characters long</span>}
-                    {!pwMatch(form.conPassword) && form.conPassword.length > 0 && <span style={{color:"red"}}>Passwords must match</span>}
+                    {!pwValid(form.conPassword) && form.conPassword.length > 0 && <span className="danger">Password must be 8 characters long</span>}
+                    <div>
+                        {!pwMatch(form.conPassword) && form.conPassword.length > 0 && <span className="danger">Passwords must match</span>}
+                    </div>
                     <div>
                         <label htmlFor="conPassword">Confirm Password</label>
                         <input name="conPassword" type="password" onChange={onChangeHandler} />
                     </div>
 
+
                     {
                         allValid(form)
-                        ? <input type="submit" value="Create"></input>
-                        : <input type="submit" value="Create" disabled></input>
+                            ? <input type="submit" value="Create"></input>
+                            : <input type="submit" value="Create" disabled></input>
                     }
 
                 </div>
